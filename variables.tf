81f3d0af8a71bc1e230e2dd8705322bd2f129e69
SHAHIEN Alshaimaa OBS/GDO <dbfg6185@orange.com>
2023-10-10 10:04:25 +0200

    variables

---
+++ variables.tf
@@ -0,0 +1,63 @@
+variable ACCESS_KEY  {
+    type = string
+}
+
+variable SECRET_KEY  {
+    type = string
+}
+
+variable REGION {
+    type = string
+}
+
+variable DOMAIN_NAME {
+    type = string
+}
+
+variable TENAT_ID {
+    type = string
+}
+
+variable VPC {
+    type = string
+}
+
+variable CIDR {
+    type = string
+}
+
+variable SUBNET {
+    type = string
+}
+
+variable SUB_CIDR {
+    type = string
+}
+
+variable GATEWAY_IP {
+    type = string
+}
+
+variable KEY_PAIR {
+    type = string
+}
+
+variable SEC_GROUP {
+    type = string
+}
+
+variable SEC_GROUP_DESCRIPTION {
+    type = string
+}
+
+variable ECS {
+    type = string
+}
+
+variable IMAGE_NAME {
+    type = string
+}
+
+variable FLAVOR_NAME {
+    type = string
+}
